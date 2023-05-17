a2f58c499d8e45d4616d09c02a604bb786b92647
Emmanuel-Youpy <emmydeji58@gmail.com>
2023-05-16 22:56:20 -0500

    HomePage Screen

---
+++ screens/HomeScreen.js
@@ -0,0 +1,54 @@
+import { View, Text, SafeAreaView, Image, TouchableOpacity } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: false,
+    });
+  }, []);
+  return (
+    <SafeAreaView className=" flex-1 relative">
+      {/* First Section */}
+      <View className="flex-row px-6 mt-8 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#4DABB7] text-3xl font-semibold">Go</Text>
+        </View>
+        <Text className="text-[#2A2B4B] text-3xl font-semibold"> Travel</Text>
+      </View>
+      {/* Second Section */}
+      <View className="px-6 mt-8 space-y-3">
+        <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+        <Text className="text-[#00BCC9] text-[38px]">Good Moments</Text>
+        <Text className="text-[#3C6072] text-base">
+          A journey is like a marriage. The certain way to be wrong is to think
+          you control it.
+        </Text>
+      </View>
+
+      {/* 3rd SEction */}
+      <View className="w-[400px] h-[400px] rounded-full absolute bg-[#00BCC9] bottom-36 -right-36"></View>
+      <View className="w-[400px] h-[400px] rounded-full absolute bg-[#E99265] -bottom-28 -left-36"></View>
+      {/* Image */}
+      <View className="flex-1  items-center justify-center mt-5">
+        <Image
+          source={require("../assets/hero.png")}
+          className="w-full h-full object-cover"
+        />
+          <View className="absolute bottom-20 w-24 h-24 border-l-2 border-r-2 border-t-4 border-[#00BCC9]  rounded-full items-center justify-center">
+            <TouchableOpacity>
+        <View className="w-20 h-20 items-center justify-center rounded-full bg-[#00BCC9]">
+            <Text className="text-gray-50 text-[36px] font-semibold">GO</Text>
+        </View>
+        </TouchableOpacity>
+      </View>
+      </View>
+    
+    </SafeAreaView>
+  );
+};
+
+export default HomeScreen;
