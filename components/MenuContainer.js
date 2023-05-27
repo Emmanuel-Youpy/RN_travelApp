9496e20d95fe65d178c9b67f468c41a5ac48211c
Emmanuel-Youpy <emmydeji58@gmail.com>
2023-05-26 22:57:06 -0500

    Menu component contd

--- components/MenuContainer.js
+++ components/MenuContainer.js
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
 
-const MenuContainer = () => {
+const MenuContainer = ({ title, imageSrc, type, setType }) => {
   return (
-    <View>
-      <Text>MenuContainer</Text>
-    </View>
-  )
-}
+    <TouchableOpacity className="items-center justify-center space-y-2">
+      <View
+        className={`w-24 h-24 shadow-sm rounded-full items-center justify-center ${
+          type === title.toLowerCase() ? "bg-gray-200" : ""
+        }`}
+      >
+        <Image source={imageSrc} className="w-full h-full object-contain" />
+      </View>
+      <Text className="text-[#00DCC9] text-xl font-semibold">{title}</Text>
+    </TouchableOpacity>
+  );
+};
 
-export default MenuContainer
+export default MenuContainer;
--- screens/Discover.js
+++ screens/Discover.js
@@ -1,12 +1,11 @@
 import { View, Text, SafeAreaView, Image, ScrollView } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useLayoutEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import MenuContainer from "../components/MenuContainer";
 
-
 const Discover = () => {
-    const APIKEY = 'AIzaSyB8vv4xBlAUz7n0jksvMuWQlQUvkIR7Bqw'
+  const APIKEY = "AIzaSyB8vv4xBlAUz7n0jksvMuWQlQUvkIR7Bqw";
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
@@ -14,40 +13,47 @@ const Discover = () => {
       headerShown: false,
     });
   }, []);
+  const [type, setType] = useState("resturants");
   return (
     <SafeAreaView className="flex-1 bg-white relative">
       <View className="flex-row items-center justify-between px-8">
         <View>
           <Text className="text-[#0B646B] text-[40px] font-bold">Discover</Text>
-          <Text className="text-[#527283] text-[36px] ">the beauty todaygg</Text>
+          <Text className="text-[#527283] text-[36px] ">
+            the beauty todaygg
+          </Text>
         </View>
-        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center "> 
-        <Image
-          source={require("../assets/avatar.png")}
-          className="w-full h-full object-cover"
-        />
-
+        <View className="w-12 h-12 bg-gray-400 rounded-md items-center justify-center ">
+          <Image
+            source={require("../assets/avatar.png")}
+            className="w-full h-full object-cover"
+          />
         </View>
       </View>
       <View className="flex-row items-center bg-white mx-4 rounded-xl px-4 py-1 mt-4 shadow-lg">
-      <GooglePlacesAutocomplete
-      placeholder='Search'
-      onPress={(data, details = null) => {
-        // 'details' is provided when fetchDetails = true
-        console.log(data, details);
-      }}
-      query={{
-        key: APIKEY,
-        language: 'en',
-      }}
-    />
-
+        <GooglePlacesAutocomplete
+          placeholder="Search"
+          onPress={(data, details = null) => {
+            // 'details' is provided when fetchDetails = true
+            console.log(data, details);
+          }}
+          query={{
+            key: APIKEY,
+            language: "en",
+          }}
+        />
       </View>
 
       {/* Menu Container */}
       <ScrollView>
         <View className="flex-row items-center justify-center px-8 mt-8">
-<MenuContainer/>
+          <MenuContainer
+            key={"hotel"}
+            title="Hotels"
+            imageSrc={'../assets/hotel.png'}
+            type={type}
+            setType={setType}
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
